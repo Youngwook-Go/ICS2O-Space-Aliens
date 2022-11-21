e26e001f48a593cfe3969bb7aed9b3e411e543fe
Youngwook Go <113055584+Youngwook-Go@users.noreply.github.com>
2022-11-21 19:17:51 +0000

    #05-1

--- js/splashScene.js
+++ js/splashScene.js
@@ -18,6 +18,7 @@ class SplashScene extends Phaser.Scene
   constructor()
   {
     super({ key: "splashScene" })
+    this.splashSceneBackgroundImage = null
   }
   init(data) 
 
@@ -28,16 +29,23 @@ class SplashScene extends Phaser.Scene
   preload()
   {
     console.log("Splash Scene")
+    this.load.image("splashSceneBackground", "./assets/splashSceneImage.png")
   }
   
   create(data) 
   {
-    //pass
+    this.splashSceneBackgroundImage = this.add.sprite(
+      0, 0, "splashSceneBackground"
+    )
+    this.splashSceneBackgroundImage.x = 1920 / 2
+    this.splashSceneBackgroundImage.y = 1080 / 2
   }
 
   update(time, delta)
   {
+    if (time > 3000) {
     this.scene.switch("titleScene")
+    }
   }
 }
 
