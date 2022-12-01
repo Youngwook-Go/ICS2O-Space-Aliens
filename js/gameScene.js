f533b9ce445c73081a6ff17f4bb244934e10dad2
Youngwook Go <113055584+Youngwook-Go@users.noreply.github.com>
2022-12-01 03:22:01 +0000

    #08-1

--- js/gameScene.js
+++ js/gameScene.js
@@ -16,6 +16,9 @@ class GameScene extends Phaser.Scene {
   constructor () 
   {
     super({ key: "gameScene" })
+
+    this.background = null
+    this.ship = null
   }
   
   init(data) 
@@ -26,11 +29,17 @@ class GameScene extends Phaser.Scene {
   preload()
   {
     console.log("Game Scene")
+    
+    this.load.image("starBackground", "./assets/starBackground.png")
+    this.load.image("ship", "./assets/spaceShip.png")
   }
   
   create(data) 
   {
-    // pass
+    this.background = this.add.image(0, 0, "starBackground").setScale(2.0)
+    this.background.setOrigin(0, 0)
+
+    this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
   }
 
   update(time, delta)
