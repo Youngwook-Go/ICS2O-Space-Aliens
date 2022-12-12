86c16080daf85e66732f062bf5fbc042e631ccc5
Youngwook Go <113055584+Youngwook-Go@users.noreply.github.com>
2022-12-12 23:42:42 +0000

    #12-1

--- js/gameScene.js
+++ js/gameScene.js
@@ -13,6 +13,17 @@ class GameScene extends Phaser.Scene {
   /**
    * This method is the constructor
   **/
+  createAlien ()
+  {
+    const alienXLocation = Math.floor(Math.random() * 1920) + 1
+    let alienXVelocity = Math.floor(Math.random() * 50) + 1
+    alienXVelocity *= Math.round(Math.random()) ? 1 : -1
+    const anAlien = this.physics.add.sprite(alienXLocation, -100, 'alien')
+    anAlien.body.velocity.y = 200
+    anAlien.body.velocity.x = alienXVelocity
+    this.alienGroup.add(anAlien)
+  }
+    
   constructor () 
   {
     super({ key: 'gameScene' })
@@ -34,6 +45,8 @@ class GameScene extends Phaser.Scene {
     this.load.image('starBackground', './assets/starBackground.png')
     this.load.image('ship', './assets/spaceShip.png')
     this.load.image('missile', './assets/missile.png')
+    this.load.image('alien', './assets/alien.png')
+    
     this.load.audio('laser', './assets/laser1.wav')
   }
   
@@ -45,6 +58,9 @@ class GameScene extends Phaser.Scene {
     this.ship = this.physics.add.sprite(1920 / 2, 1080 - 100, "ship")
 
     this.missileGroup = this.physics.add.group()
+
+    this.alienGroup = this.add.group()
+    this.createAlien()
   }
 
   update(time, delta)
